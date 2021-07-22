11ad7ba8216fae9e830a4c91badacc196cf3fb22
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2021-07-22 15:39:51 -0300

    inserindo material icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../../styles/globals.css'
+import { Button } from '@material-ui/core';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Button>Hello World</Button>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
