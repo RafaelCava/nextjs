c96a112bd492956a0afc016e7669eadc7026ba3c
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2021-07-22 15:43:04 -0300

    pages/_app: retirando material icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import '../../styles/globals.css'
-import { Button } from '@material-ui/core';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <Button>Hello World</Button>
       <Component {...pageProps} />
     </div>
   )
